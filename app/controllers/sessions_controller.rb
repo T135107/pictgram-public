3c6543574b3bd7ab0d121dfa28aa65ffb8eff671
T135107 <tatsuto107@gmail.com>
2020-05-31 01:34:14 +0000

    third commit 2

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,8 @@ class SessionsController < ApplicationController
   end
   
   def create
-    user = User.find_by(email: params[:session][:email])
-    if user && user.authenticate(params[:session][:password])
+    user = User.find_by(email: user_params[:email])
+    if user && user.authenticate(user_params[:password])
       log_in user
       redirect_to root_path, success: 'ログインに成功しました'
     else
